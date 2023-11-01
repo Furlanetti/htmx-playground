32ca090183d4685ed81f9e7df7535444e7f5f495
furlanetti <felipe.furlanetti1@gmail.com>
2023-11-01 15:54:40 -0300

    Sending data to server

--- api/index.js
+++ api/index.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("<h2>Welcome Hipermedia API</h2>");
@@ -18,6 +19,16 @@ app.post("/message", async (req, res) => {
   setTimeout(() => {
     res.send("<h3>Hello world</h3>");
   }, 5000);
+});
+
+app.post("/echo_payload", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  res.send(`<div>
+    <b>Email: </b> ${email} <br />
+    <b>Password: </b> ${password}
+    </div>`);
 });
 
 const PORT = process.env.PORT || 1330;
